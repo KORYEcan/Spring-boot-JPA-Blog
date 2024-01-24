2493aecb062ece92e9ff35ae142a90fb31d0fd59
KORYEcan <philo0401@naver.com>
2024-01-25 03:49:12 +0900

    GlobalExceptionHandler 완성

--- src/main/java/com/example/blog1/test/DummyControllerTest.java
+++ src/main/java/com/example/blog1/test/DummyControllerTest.java
@@ -5,8 +5,15 @@ import com.example.blog1.model.RoleType;
 import com.example.blog1.model.User;
 import com.example.blog1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 @RestController //HTML이 아닌 데이터를 리턴해주는 컨트롤러
@@ -14,6 +21,54 @@ public class DummyControllerTest {
 
     @Autowired  //의존성 주입(DI)
     private UserRepository userRepository;
+
+
+
+    @DeleteMapping("/dummy/user/{id}")
+    public String delete(@PathVariable int id) {
+            User user = userRepository.findById(id).orElseThrow(()-> {
+                return new IllegalArgumentException("삭제할 엔티티가 존재하지 않습니다. id="+id);
+            });
+
+            userRepository.deleteById(id);
+            return "삭제되었습니다. id=" + id;
+
+    }
+
+
+    //email, password
+    @Transactional //메서드 종료시에 자동 commit이 됨.
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+        System.out.println("id = " + id);
+        System.out.println("password:" + requestUser.getPassword());
+        System.out.println("email:" + requestUser.getEmail());
+
+        User user = userRepository.findById(id).orElseThrow(()-> {
+            return new IllegalArgumentException("수정에 실패하였습니다");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+//        userRepository.save(user);
+         //더티 체킹 ->
+        return user;
+    }
+
+    // http://localhost:8000/blog/dummy/user
+    @GetMapping("/dummy/users")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+
+    //한페이지당 2건에 데이터를 리턴받아 볼 예정
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size=2,sort = "id", direction =Sort.Direction.DESC)Pageable pageable) {
+    Page<User> pagingUser= userRepository.findAll(pageable);
+    List<User> users = pagingUser.getContent();
+    return users;
+   }
 
     //http://localhost:8000/blog/dummy/user/3
     @GetMapping("/dummy/user/{id}")   //{id} 주소로 파라미터를 전달 받을 수 있음
