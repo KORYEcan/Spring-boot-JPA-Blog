252f6efb94af05e17b5305e267b5a4b7672f3fdd
KORYEcan <philo0401@naver.com>
2024-02-07 02:24:42 +0900

    UserApiController url 수정완료

--- src/main/java/com/example/blog1/controller/api/UserApiController.java
+++ src/main/java/com/example/blog1/controller/api/UserApiController.java
@@ -16,7 +16,7 @@ public class UserApiController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/api/user")
+    @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user) {   //username , password, email
         System.out.println("UserApiController:save 호출됨");
         //실제로 DB에 insert를 하고 아래에서 return이 되면 되요.
@@ -25,18 +25,6 @@ public class UserApiController {
 
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1); // 자바 오프젝트를 JSON으로 변환해서 리턴(Jackson)
     }
-
-   /* @PostMapping("/api/user/login")   //스프링 시큐리티 이용해서 로그인
-    public ResponseDto<Integer> login(@RequestBody User user , HttpSession session) {
-        System.out.println("UserApiController:login 호출됨");
-        User principal=userService.login(user);   //principal(접근주체)
-
-        if (principal != null) {
-            session.setAttribute("principal", principal);
-        }
-
-        return new ResponseDto<Integer>(HttpStatus.OK.value(),1); // 자바 오프젝트를 JSON으로 변환해서 리턴(Jackson)
-    }*/
 
 
 
