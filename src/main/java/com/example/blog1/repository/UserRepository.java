ae2a03421bcfc44d37a4a21aaa392c263a5e97e4
KORYEcan <philo0401@naver.com>
2024-02-07 02:29:14 +0900

    loginForm.jsp 로그인 버튼 수정

--- src/main/java/com/example/blog1/repository/UserRepository.java
+++ src/main/java/com/example/blog1/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.blog1.repository;
 
 import com.example.blog1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 
 //DAO
@@ -10,13 +9,15 @@ import org.springframework.data.jpa.repository.Query;
 //@Repository //생략 가능함
 public interface UserRepository extends JpaRepository<User,Integer> { // JpaRepository 제너릭을 사용해서 해당 Repository는 User객체값만 들어올수 있게끔 하는것
 
-    // JPA Naming 쿼리
-    //SELECT * FROM user WHERE username= ?1 AND password= ?2
-//    User findByUsernameAndPassword(String username, String password);
 
-//    @Query(value = "SELECT * FROM user WHERE username= ?1 AND password= ?2" , nativeQuery = true)
-//    User login(String username, String password);
 
 
 
 }
+
+
+// JPA Naming 쿼리
+//SELECT * FROM user WHERE username= ?1 AND password= ?2
+//    User findByUsernameAndPassword(String username, String password);
+//    @Query(value = "SELECT * FROM user WHERE username= ?1 AND password= ?2" , nativeQuery = true)
+//    User login(String username, String password);
--- src/main/webapp/WEB-INF/views/user/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/user/loginForm.jsp
@@ -37,8 +37,8 @@
                 <input class="form-check-input" type="checkbox"> Remember me
             </label>
         </div>
+        <button id="btn-login" class="btn btn-primary">로그인</button>
     </form>
-    <button id="btn-login" class="btn btn-primary">로그인</button>
 
 </div>
 
