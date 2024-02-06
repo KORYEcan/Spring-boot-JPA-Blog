4c486e9002da867b4bd60f9b63f00e9fa6c38e8a
KORYEcan <philo0401@naver.com>
2024-02-07 02:23:10 +0900

    UserController url 수정

--- src/main/java/com/example/blog1/controller/UserController.java
+++ src/main/java/com/example/blog1/controller/UserController.java
@@ -3,17 +3,20 @@ package com.example.blog1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//인증이 안된 사용자들이 출입할 수 있는 경로를 /auth/** 허용
+//그냥 주소가 / 이면 index.jsp 허용
+//static이하에 있는  /js/** , /css/**, /image/**
+
 @Controller
 public class UserController {
 
-    @GetMapping("/joinForm")  //회원 가입
+    @GetMapping("/auth/joinForm")  //회원 가입
      public String joinForm() {
-
         return "user/joinForm";
      }
-    @GetMapping("/loginForm")  //로그인
-     public String loginForm() {
 
+    @GetMapping("/auth/loginForm")  //로그인
+     public String loginForm() {
         return "user/loginForm";
      }
 
